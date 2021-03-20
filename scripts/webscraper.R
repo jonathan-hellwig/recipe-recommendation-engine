e25f697a28ca421bf4e10afcf515e2abadf774bd
outerTape <jonathan.hellwig@posteo.de>
2021-03-20 21:03:04 +0100

    Update webscraper

--- scripts/webscraper.R
+++ scripts/webscraper.R
@@ -3,7 +3,11 @@ library(dplyr)
 
 url <- "https://www.kurashiru.com/recipes/7a90feb7-0542-4299-adb3-9339fb75f9d7"
 
-title <- read_html(url) %>% 
-  html_node(".title") %>% 
-  html_text()
+content <- read_html(url)
 
+title <- content %>%
+  html_node(".title") %>%
+  html_text()
+cooking_time <- content %>%
+  html_node(".cooking-time") %>%
+  html_text()
